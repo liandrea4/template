a3ba540dce6d9c7a32645135c0ea74cd70a5ae2a
Andrea Iosue <li.andrea4@gmail.com>
2023-01-11 22:51:41 -0500

    change database settings

--- template/settings.py
+++ template/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -88,13 +89,24 @@ WSGI_APPLICATION = 'template.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "github_actions",
-        "USER": "postgres",
-        "PASSWORD": "postgres",
+        "NAME": "citi",
+        "USER": "iosue",
+        "PASSWORD": "password",
         "HOST": "localhost",
-        "PORT": "5432",
+        "PORT": "",
     }
 }
+if os.environ.get("GITHUB_WORKFLOW"):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "github_actions",
+            "USER": "postgres",
+            "PASSWORD": "postgres",
+            "HOST": "127.0.0.1",
+            "PORT": "5432",
+        }
+    }
 
 
 # Password validation
